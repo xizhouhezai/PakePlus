3b0f20914f7711108726ff502047381350ba289b
柏舟 <yangrui1372106119@gmail.com>
2025-04-17 09:27:06 +0800

    update src file from pakeplus

---
+++ src/assets/index-Cup18t2O.js
@@ -0,0 +1,1 @@
+import{d as o,_ as n,b as t,o as r,j as s}from"./index-C4AMdMz2.js";const c=o({});function a(_,p,i,d,f,m){const e=t("router-view");return r(),s(e)}const l=n(c,[["render",a]]);export{l as default};
