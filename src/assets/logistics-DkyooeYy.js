d78f8fc1931dd347110d5d6da503dfe2962296a8
柏舟 <yangrui1372106119@gmail.com>
2025-04-17 09:30:41 +0800

    update src file from pakeplus

---
+++ src/assets/logistics-DkyooeYy.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./logistics.vue_vue_type_script_setup_true_lang-BnF_DKPT.js";import"./operation-CQQGRn8-.js";import"./index-C4AMdMz2.js";import"./validator-B8pIxTAL.js";export{o as default};
